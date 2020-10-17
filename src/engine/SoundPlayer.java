697f210d14913b30ccb8f6f64f919ac93707cc9c
bigglopes <guilhermeslfonseca@gmail.com>
2020-10-17 20:32:47 -0300

    Ajuste para musica funcionar dentro do jar.

--- src/engine/SoundPlayer.java
+++ src/engine/SoundPlayer.java
@@ -1,5 +1,8 @@
 package engine;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -9,7 +12,13 @@ public class SoundPlayer {
 	private Clip clip;
 
 	public void playMusic(String music) throws Exception {
-		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getResourceAsStream( music ));
+
+		DataInputStream dis = new DataInputStream(getClass().getResourceAsStream(music));
+		byte[] musica = new byte[dis.available()];
+		dis.readFully(musica);
+		dis.close();
+		ByteArrayInputStream bis = new ByteArrayInputStream(musica);
+		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bis);
 		clip = AudioSystem.getClip();
 		clip.open(audioInputStream);
 		clip.start();
